6a9fc6732009e3ef554890157bfd0c8d07e76d2d
lemuria-pbem <lemuria@online.de>
2020-12-31 17:30:57 +0100

    Clean up.

--- src/Message/Unit/ContactNotFoundMessage.php
+++ src/Message/Unit/ContactNotFoundMessage.php
@@ -5,17 +5,15 @@ namespace Lemuria\Engine\Lemuria\Message\Unit;
 use Lemuria\Engine\Lemuria\Message\LemuriaMessage;
 use Lemuria\Engine\Message;
 
-class ContactNotFoundMessage extends UnitMessage
+class ContactNotFoundMessage extends ContactMessage
 {
 	protected string $level = Message::FAILURE;
 
-	protected string $contact;
-
 	protected function create(): string {
-		return 'Unit ' . $this->id . ' cannot find unit ' . $this->contact . ' to contact.';
+		return 'Unit ' . $this->id . ' cannot find unit ' . $this->unit . ' to contact.';
 	}
 
 	protected function getData(LemuriaMessage $message): void {
-		$this->contact = $message->getParameter();
+		$this->unit = $message->getParameter();
 	}
 }
