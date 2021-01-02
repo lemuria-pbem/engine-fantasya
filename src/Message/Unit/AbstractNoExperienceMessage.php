c5344701c7dbeca37b798d05ac21aaae63b97e34
lemuria-pbem <lemuria@online.de>
2021-01-02 11:53:41 +0100

    Bugfixing.

--- src/Message/Unit/AbstractNoExperienceMessage.php
+++ src/Message/Unit/AbstractNoExperienceMessage.php
@@ -5,6 +5,7 @@ namespace Lemuria\Engine\Lemuria\Message\Unit;
 use Lemuria\Engine\Message;
 use Lemuria\Model\Lemuria\Factory\BuilderTrait;
 use Lemuria\Model\Lemuria\Talent;
+use Lemuria\Singleton;
 
 abstract class AbstractNoExperienceMessage extends AbstractUnitMessage
 {
@@ -12,9 +13,19 @@ abstract class AbstractNoExperienceMessage extends AbstractUnitMessage
 
 	protected string $level = Message::FAILURE;
 
+	protected Singleton $talent;
+
+	public function __construct() {
+		$this->talent = $this->getTalent();
+	}
+
 	protected function create(): string {
-		return 'Unit ' . $this->id . ' has no experience in ' . $this->getTalent() . '.';
+		return 'Unit ' . $this->id . ' has no experience in ' . $this->talent . '.';
 	}
 
 	abstract protected function getTalent(): Talent;
+
+	protected function getTranslation(string $name): string {
+		return $this->talent($name, 'talent') ?? parent::getTranslation($name);
+	}
 }
