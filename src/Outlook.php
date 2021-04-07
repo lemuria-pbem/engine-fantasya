426c893aa6ed769e3c8ad20402130946c0cf6414
sascha.ternes <sascha.ternes@diemedialen.de>
2021-04-07 09:16:57 +0200

    Bugfixing.

--- src/Outlook.php
+++ src/Outlook.php
@@ -41,7 +41,7 @@ final class Outlook
 		$camouflage = self::createTalent(Camouflage::class);
 		foreach ($region->Residents() as $unit /* @var Unit $unit */) {
 			if (!$unit->Construction() && !$unit->Vessel()) {
-				if ($unit->Party() === $party) {
+				if ($unit->Party() === $party || !$unit->IsHiding()) {
 					$units->add($unit);
 				} else {
 					$calculus = new Calculus($unit);
