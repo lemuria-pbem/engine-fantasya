b89ce664bc0b7c081f09231259f180243d954040
lemuria-pbem <lemuria@online.de>
2021-07-03 20:48:49 +0200

    Bugfixing.

--- src/Command/Create/Herb.php
+++ src/Command/Create/Herb.php
@@ -6,11 +6,15 @@ use Lemuria\Engine\Fantasya\Context;
 use Lemuria\Engine\Fantasya\Factory\Model\Job;
 use Lemuria\Engine\Fantasya\Message\Unit\HerbUnknownMessage;
 use Lemuria\Engine\Fantasya\Phrase;
-use Lemuria\Model\Fantasya\Herbage;
 use Lemuria\Engine\Fantasya\Factory\Model\Herb as HerbModel;
 use Lemuria\Exception\LemuriaException;
+use Lemuria\Model\Fantasya\Ability;
 use Lemuria\Model\Fantasya\Commodity;
+use Lemuria\Model\Fantasya\Commodity\Herb\Elvendear;
 use Lemuria\Model\Fantasya\Herb as HerbInterface;
+use Lemuria\Model\Fantasya\Herbage;
+use Lemuria\Model\Fantasya\Talent;
+use Lemuria\Model\Fantasya\Talent\Herballore;
 
 /**
  * Implementation of command MACHEN Kräuter (create herb).
@@ -24,7 +28,8 @@ final class Herb extends RawMaterial
 
 	public function __construct(Phrase $phrase, Context $context, Job $job) {
 		parent::__construct($phrase, $context, $job);
-		$this->herbage = $this->unit->Party()->HerbalBook()->getHerbage($this->unit->Region());
+		$this->knowledge = new Ability(self::createTalent(Herballore::class), Ability::getExperience(3));
+		$this->herbage   = $this->unit->Party()->HerbalBook()->getHerbage($this->unit->Region());
 	}
 
 	/**
@@ -41,11 +46,17 @@ final class Herb extends RawMaterial
 	protected function getCommodity(): Commodity {
 		$resource = $this->job->getObject();
 		if ($resource instanceof HerbModel) {
-			return $this->herbage->Herb();
+			return $this->herbage?->Herb() ?? self::createCommodity(Elvendear::class); // Dummy
 		}
 		if ($resource instanceof HerbInterface) {
 			return $resource;
 		}
 		throw new LemuriaException($resource . ' is not a herb.');
+	}
+
+	protected function runForEmptyDemand(Talent $talent, Commodity $resource): void {
+		if ($this->herbage) {
+			parent::runForEmptyDemand($talent, $resource);
+		}
 	}
 }
