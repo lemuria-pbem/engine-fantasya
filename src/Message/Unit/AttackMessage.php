230b28e081c8d81829afdcceaaa898e91e350c21
lemuria-pbem <lemuria@online.de>
2021-07-28 22:28:59 +0200

    Bugfixing.

--- src/Message/Unit/AttackMessage.php
+++ src/Message/Unit/AttackMessage.php
@@ -2,13 +2,22 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Message\Unit;
 
+use Lemuria\Engine\Fantasya\Message\LemuriaMessage;
 use Lemuria\Engine\Message;
+use Lemuria\Id;
 
-class AttackMessage extends AttackOwnUnitMessage
+class AttackMessage extends AbstractUnitMessage
 {
 	protected string $level = Message::SUCCESS;
 
+	protected Id $unit;
+
 	protected function create(): string {
 		return 'Unit ' . $this->id . ' attacks unit ' . $this->unit . '.';
+	}
+
+	protected function getData(LemuriaMessage $message): void {
+		parent::getData($message);
+		$this->unit = $message->get();
 	}
 }
