844457203ddbd2db1cc0c607090aecc6497f42ce
lemuria-pbem <lemuria@online.de>
2022-03-18 08:29:04 +0100

    Add retirement from the game after the last unit is lost.

---
+++ src/Message/Party/RetirementMessage.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Engine\Fantasya\Message\Party;
+
+use Lemuria\Engine\Message;
+use Lemuria\Engine\Message\Section;
+
+class RetirementMessage extends AbstractPartyMessage
+{
+	protected string $level = Message::FAILURE;
+
+	protected Section $section = Section::ECONOMY;
+
+	protected function create(): string {
+		return 'You have lost your last unit, and retire. This will be your last report. Farewell!';
+	}
+}
