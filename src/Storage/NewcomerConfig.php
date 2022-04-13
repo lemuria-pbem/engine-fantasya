22d385f0d5b50cec5623ef24415911b023a8ef48
lemuria-pbem <lemuria@online.de>
2022-04-13 11:59:08 +0200

    Add newcomer statistics dummy.

--- src/Storage/NewcomerConfig.php
+++ src/Storage/NewcomerConfig.php
@@ -2,7 +2,10 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Storage;
 
+use JetBrains\PhpStorm\Pure;
+
 use Lemuria\Model\Game;
+use Lemuria\Statistics;
 
 /**
  * A special game configuration that ignores all changes to game data but newcomer editing.
@@ -12,4 +15,9 @@ class NewcomerConfig extends LemuriaConfig
 	public function Game(): Game {
 		return new NewcomerGame($this);
 	}
+
+	#[Pure] public function Statistics(): Statistics {
+		return new NewcomerStatistics();
+	}
+
 }
