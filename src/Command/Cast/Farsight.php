1f5488ca0a3bd461c10f0331b5b759d9d2823935
lemuria-pbem <lemuria@online.de>
2022-04-20 14:24:59 +0200

    Respect Farsight effect in outlook apparitions.

--- src/Command/Cast/Farsight.php
+++ src/Command/Cast/Farsight.php
@@ -3,17 +3,24 @@ declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Command\Cast;
 
 use Lemuria\Engine\Fantasya\Effect\FarsightEffect;
+use Lemuria\Engine\Fantasya\Effect\TalentEffect;
 use Lemuria\Engine\Fantasya\Factory\MessageTrait;
 use Lemuria\Engine\Fantasya\Message\Unit\Cast\FarsightMessage;
 use Lemuria\Engine\Fantasya\Message\Unit\Cast\FarsightTooFarMessage;
 use Lemuria\Engine\Fantasya\Message\Unit\Cast\FarsightUnknownMessage;
 use Lemuria\Engine\Fantasya\State;
 use Lemuria\Lemuria;
-use Lemuria\Model\Fantasya\Party;
+use Lemuria\Model\Fantasya\Ability;
+use Lemuria\Model\Fantasya\Factory\BuilderTrait;
+use Lemuria\Model\Fantasya\Modification;
 use Lemuria\Model\Fantasya\Region;
+use Lemuria\Model\Fantasya\Talent;
+use Lemuria\Model\Fantasya\Talent\Perception;
+use Lemuria\Model\Fantasya\Unit;
 
 final class Farsight extends AbstractCast
 {
+	use BuilderTrait;
 	use MessageTrait;
 
 	public function cast(): void {
@@ -26,8 +33,7 @@ final class Farsight extends AbstractCast
 			}
 			$distance = Lemuria::World()->getDistance($unit->Region(), $region);
 
-			$party     = $unit->Party();
-			$chronicle = $party->Chronicle();
+			$chronicle = $unit->Party()->Chronicle();
 			if (!$chronicle->has($region->Id())) {
 				$this->message(FarsightUnknownMessage::class, $unit)->e($region);
 				return;
@@ -37,7 +43,7 @@ final class Farsight extends AbstractCast
 			if ($unit->Aura()->Aura() >= $aura) {
 				$unit->Aura()->consume($aura);
 				$chronicle->add($region);
-				$this->addEffect($region, $party);
+				$this->addEffect($region, $unit);
 				$this->message(FarsightMessage::class, $unit)->e($region);
 			} else {
 				$this->message(FarsightTooFarMessage::class, $unit)->e($region);
@@ -45,7 +51,7 @@ final class Farsight extends AbstractCast
 		}
 	}
 
-	private function addEffect(Region $region, Party $party): void {
+	private function addEffect(Region $region, Unit $unit): void {
 		$effect   = new FarsightEffect(State::getInstance());
 		$existing = Lemuria::Score()->find($effect->setRegion($region));
 		if (!$existing) {
@@ -53,6 +59,34 @@ final class Farsight extends AbstractCast
 		} else {
 			$effect = $existing;
 		}
-		$effect->Parties()->add($party);
+
+		$perception = self::createTalent(Perception::class);
+		/** @var Ability $ability */
+		$ability      = $unit->Knowledge()->offsetGet($perception);
+		$modification = $unit->Race()->Modifications()->offsetGet($perception);
+		if ($modification instanceof Modification) {
+			$ability = $modification->getModified($ability);
+		}
+		$modification = $unit->Race()->TerrainEffect()->getEffect($region->Landscape(), $perception);
+		if ($modification instanceof Modification) {
+			$ability = $modification->getModified($ability);
+		}
+		$modification = $this->talentEffect($unit, $perception);
+		if ($modification instanceof Modification) {
+			$ability = $modification->getModified($ability);
+		}
+		$effect->addParty($unit->Party(), $ability->Level());
+	}
+
+	private function talentEffect(Unit $unit, Talent $talent): ?Modification {
+		$effect   = new TalentEffect(State::getInstance());
+		$existing = Lemuria::Score()->find($effect->setUnit($unit));
+		if ($existing instanceof TalentEffect) {
+			$modifications = $existing->Modifications();
+			if (isset($modifications[$talent])) {
+				return $modifications[$talent];
+			}
+		}
+		return null;
 	}
 }
--- src/Effect/FarsightEffect.php
+++ src/Effect/FarsightEffect.php
@@ -10,11 +10,17 @@ use Lemuria\Engine\Fantasya\State;
 use Lemuria\Exception\UnserializeEntityException;
 use Lemuria\Lemuria;
 use Lemuria\Model\Fantasya\Gathering;
+use Lemuria\Model\Fantasya\Party;
 use Lemuria\Serializable;
 
 final class FarsightEffect extends AbstractRegionEffect
 {
 	private Gathering $parties;
+
+	/**
+	 * @var array(int=>int)
+	 */
+	private array $perception = [];
 
 	public function __construct(State $state) {
 		parent::__construct($state, Priority::BEFORE);
@@ -35,6 +41,22 @@ final class FarsightEffect extends AbstractRegionEffect
 	public function unserialize(array $data): Serializable {
 		parent::unserialize($data);
 		$this->parties->unserialize($data['parties']);
+		return $this;
+	}
+
+	#[Pure] public function getPerception(Party $party): int {
+		$id = $party->Id()->Id();
+		return $this->perception[$id] ?? 0;
+	}
+
+	public function addParty(Party $party, int $perception): FarsightEffect {
+		$this->parties->add($party);
+		$id = $party->Id()->Id();
+		if (isset($this->perception[$id])) {
+			$this->perception[$id] = max($this->perception[$id], $perception);
+		} else {
+			$this->perception[$id] = $perception;
+		}
 		return $this;
 	}
 
