168285e866656522eca93b8f1b3596fff077e14f
lemuria-pbem <lemuria@online.de>
2022-07-23 15:12:21 +0200

    Display gift from foreigners as event message.

--- src/Message/Unit/GiveReceivedFromForeignMessage.php
+++ src/Message/Unit/GiveReceivedFromForeignMessage.php
@@ -2,8 +2,12 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Message\Unit;
 
+use Lemuria\Engine\Message;
+
 class GiveReceivedFromForeignMessage extends GiveReceivedMessage
 {
+	protected string $level = Message::EVENT;
+
 	protected function create(): string {
 		return 'Unit ' . $this->id . ' receives ' . $this->gift . ' from unit ' . $this->from . '.';
 	}
