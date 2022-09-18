fc62284a9c720fe763ecc84a8cb69ccf475890d5
lemuria-pbem <lemuria@online.de>
2022-09-18 14:30:07 +0200

    [BUGFIX] Allow party ID 0 as alias for TARNEN Partei. Bugfixing.

--- src/Message/Unit/DisguiseUnknownPartyMessage.php
+++ src/Message/Unit/DisguiseUnknownPartyMessage.php
@@ -2,13 +2,21 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Message\Unit;
 
+use Lemuria\Engine\Fantasya\Message\LemuriaMessage;
 use Lemuria\Engine\Message;
 
-class DisguiseUnknownPartyMessage extends DisguiseKnownPartyMessage
+class DisguiseUnknownPartyMessage extends AbstractUnitMessage
 {
 	protected string $level = Message::FAILURE;
 
+	protected string $party;
+
 	protected function create(): string {
 		return 'Unit ' . $this->id . ' cannot claim belonging to unknown party ' . $this->party . '.';
+	}
+
+	protected function getData(LemuriaMessage $message): void {
+		parent::getData($message);
+		$this->party = $message->getParameter();
 	}
 }
