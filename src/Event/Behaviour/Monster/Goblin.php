c51a631acb6c13780a6fd22a328e6028e1802ad4
lemuria-pbem <lemuria@online.de>
2023-02-27 22:42:24 +0100

    [BUGFIX] Add a global maximum for goblin population.

--- src/Event/Behaviour/Monster/Goblin.php
+++ src/Event/Behaviour/Monster/Goblin.php
@@ -9,11 +9,17 @@ use Lemuria\Engine\Fantasya\Event\Behaviour;
 use Lemuria\Engine\Fantasya\Event\Behaviour\AbstractBehaviour;
 use Lemuria\Engine\Fantasya\Event\Reproduction;
 use Lemuria\Lemuria;
+use Lemuria\Model\Fantasya\Commodity\Monster\Goblin as Model;
 use Lemuria\Model\Fantasya\Commodity\Peasant;
+use Lemuria\Model\Fantasya\Factory\BuilderTrait;
+use Lemuria\Model\Fantasya\Party;
+use Lemuria\Model\Fantasya\Party\Type;
 use Lemuria\Model\Fantasya\Unit;
 
 class Goblin extends AbstractBehaviour
 {
+	use BuilderTrait;
+
 	protected const RATE = 0.33;
 
 	protected const VARIANCE = 0.2;
@@ -28,11 +34,18 @@ class Goblin extends AbstractBehaviour
 
 	protected const SCATTER_PERSONS = 31;
 
+	protected const GLOBAL_RATE = 0.33;
+
+	protected static ?int $globalMaximum = null;
+
 	protected Reproduction $reproduction;
 
 	public function __construct(Unit $unit) {
 		parent::__construct($unit);
 		$this->reproduction = new Reproduction($this->race());
+		if (self::$globalMaximum === null) {
+			$this->initGlobalMaximum();
+		}
 	}
 
 	/**
@@ -71,6 +84,25 @@ class Goblin extends AbstractBehaviour
 		return $this->scatter(self::SCATTER_UNITS, self::SCATTER_PERSONS);
 	}
 
+	protected function initGlobalMaximum(): void {
+		$goblins = 0;
+		$players = 0;
+		$race    = self::createMonster(Model::class);
+		foreach (Party::all() as $party) {
+			$type = $party->Type();
+			if ($type === Type::Player) {
+				$players += $party->People()->Size();
+			} elseif ($type === Type::Monster) {
+				foreach ($party->People() as $unit) {
+					if ($unit->Race() === $race) {
+						$goblins += $unit->Size();
+					}
+				}
+			}
+		}
+		self::$globalMaximum = (int)round($players * self::GLOBAL_RATE) - $goblins;
+	}
+
 	protected function calculateReproductionSize(): int {
 		$region     = $this->unit->Region();
 		$neighbours = Lemuria::World()->getNeighbours($region);
@@ -85,11 +117,14 @@ class Goblin extends AbstractBehaviour
 		$kinsmen   = $calculus->getKinsmen();
 		$relatives = $calculus->getRelatives();
 		if ($kinsmen->count() + $relatives->count() < self::MAX_UNITS - 1) {
-			$total    = $this->unit->Size() + $kinsmen->Size() + $relatives->Size();
-			$variance = 2.0 * self::VARIANCE * randFloat() - self::VARIANCE;
-			$goblins  = (int)floor(self::RATE * (1.0 + $variance) * $total);
-			$maximum  = (int)floor(self::MAXIMUM * ($peasants + $units));
-			return min($goblins, $maximum);
+			$total     = $this->unit->Size() + $kinsmen->Size() + $relatives->Size();
+			$variance  = 2.0 * self::VARIANCE * randFloat() - self::VARIANCE;
+			$goblins   = (int)floor(self::RATE * (1.0 + $variance) * $total);
+			$maximum   = (int)floor(self::MAXIMUM * ($peasants + $units));
+			$reproduce = min($goblins, $maximum);
+			if ($reproduce <= self::$globalMaximum) {
+				return $reproduce;
+			}
 		}
 		return 0;
 	}
