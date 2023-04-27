fbc9bfcf6e7f96ade0fd15da421f188ebe7a4d56
lemuria-pbem <lemuria@online.de>
2023-04-27 08:37:04 +0200

    [TASK] Add monsters for turn 111.

--- src/Event/Timer.php
+++ src/Event/Timer.php
@@ -2,16 +2,13 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Event;
 
-use Lemuria\Engine\Fantasya\Event\Game\BrokenCarriage;
 use Lemuria\Engine\Fantasya\Event\Game\FindWallet;
 use Lemuria\Engine\Fantasya\Event\Game\GoblinPlague;
-use Lemuria\Engine\Fantasya\Event\Game\PopulateContinent;
 use Lemuria\Engine\Fantasya\Event\Game\Spawn;
-use Lemuria\Engine\Fantasya\Event\Game\TheWildHunt;
 use Lemuria\Engine\Fantasya\Priority;
 use Lemuria\Engine\Fantasya\State;
 use Lemuria\Lemuria;
-use Lemuria\Model\Fantasya\Commodity\Monster\Wolf;
+use Lemuria\Model\Fantasya\Commodity\Monster\Zombie;
 use Lemuria\Model\Fantasya\Party\Type;
 
 /**
@@ -20,13 +17,8 @@ use Lemuria\Model\Fantasya\Party\Type;
 final class Timer extends DelegatedEvent
 {
 	private const SCHEDULE = [
-		90 => [
-			['class' => Spawn::class, 'options' => [Spawn::TYPE => Type::Monster, Spawn::REGION => 39, Spawn::SIZE => 8, Spawn::RACE => Wolf::class]],
-			['class' => PopulateContinent::class, 'options' => [PopulateContinent::CONTINENT => 2, PopulateContinent::CHANCES => [Wolf::class => 7]]],
-			['class' => BrokenCarriage::class, 'options' => [BrokenCarriage::PARTY => 8, BrokenCarriage::REGION => 1617]],
-			['class' => TheWildHunt::class, 'options' => [TheWildHunt::UNIT => 1573]]
-		],
 		111 => [
+			['class' => Spawn::class, 'options' => [Spawn::TYPE => Type::Monster, Spawn::REGION => 3877, Spawn::SIZE => 32, Spawn::RACE => Zombie::class]],
 			['class' => FindWallet::class, 'options' => [FindWallet::UNIT => 1669, FindWallet::SILVER => 400]],
 			['class' => FindWallet::class, 'options' => [FindWallet::UNIT => 4411, FindWallet::SILVER => 250]],
 			['class' => GoblinPlague::class, 'options' => [GoblinPlague::REGION => 61, GoblinPlague::DURATION => 2]]
