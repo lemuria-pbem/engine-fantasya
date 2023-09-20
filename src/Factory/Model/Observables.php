29012c63af61c72acfce97e7186725e6919364bb
lemuria-pbem <lemuria@online.de>
2023-09-20 19:07:05 +0200

    [FEATURE] Add weapons, armor and shield to observables.

--- src/Factory/Model/Observables.php
+++ src/Factory/Model/Observables.php
@@ -3,14 +3,16 @@ declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Factory\Model;
 
 use Lemuria\Model\Fantasya\Commodity;
+use Lemuria\Model\Fantasya\Protection;
 use Lemuria\Model\Fantasya\Quantity;
 use Lemuria\Model\Fantasya\Resources;
 use Lemuria\Model\Fantasya\Transport;
+use Lemuria\Model\Fantasya\Weapon;
 
 class Observables extends Resources
 {
 	public static function isObservable(Commodity $commodity): bool {
-		return $commodity instanceof Transport;
+		return $commodity instanceof Transport || $commodity instanceof Weapon || $commodity instanceof Protection;
 	}
 
 	public function __construct(Resources $resources) {
