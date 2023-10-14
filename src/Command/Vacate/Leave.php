11dab309beb56ea1bf026992a04ba91bfa85a907
lemuria-pbem <lemuria@online.de>
2023-10-14 10:47:37 +0200

    [FEATURE] Add automated loss of inventory for units that abandon a ship at sea.

--- src/Command/Vacate/Leave.php
+++ src/Command/Vacate/Leave.php
@@ -4,6 +4,7 @@ namespace Lemuria\Engine\Fantasya\Command\Vacate;
 
 use Lemuria\Engine\Fantasya\Command\UnitCommand;
 use Lemuria\Engine\Fantasya\Effect\UnpaidDemurrage;
+use Lemuria\Engine\Fantasya\Factory\GiftTrait;
 use Lemuria\Engine\Fantasya\Factory\SiegeTrait;
 use Lemuria\Engine\Fantasya\Message\Construction\LeaveNewOwnerMessage;
 use Lemuria\Engine\Fantasya\Message\Construction\LeaveNoOwnerMessage;
@@ -16,6 +17,13 @@ use Lemuria\Engine\Fantasya\Message\Vessel\LeaveNewCaptainMessage;
 use Lemuria\Engine\Fantasya\Message\Vessel\LeaveNoCaptainMessage;
 use Lemuria\Engine\Fantasya\State;
 use Lemuria\Lemuria;
+use Lemuria\Model\Fantasya\Commodity;
+use Lemuria\Model\Fantasya\Commodity\Silver;
+use Lemuria\Model\Fantasya\Luxury;
+use Lemuria\Model\Fantasya\Navigable;
+use Lemuria\Model\Fantasya\Protection;
+use Lemuria\Model\Fantasya\Quantity;
+use Lemuria\Model\Fantasya\RawMaterial;
 
 /**
  * A unit leaves the construction or vessel using the Leave command.
@@ -24,6 +32,7 @@ use Lemuria\Lemuria;
  */
 final class Leave extends UnitCommand
 {
+	use GiftTrait;
 	use SiegeTrait;
 
 	protected function run(): void {
@@ -56,10 +65,68 @@ final class Leave extends UnitCommand
 					} else {
 						$this->message(LeaveNoCaptainMessage::class);
 					}
+					if ($this->unit->Region()->Landscape() instanceof Navigable) {
+						$this->loseExcessInventoryAtSea();
+					}
 				}
 			} else {
 				$this->message(LeaveNotMessage::class);
 			}
 		}
+	}
+
+	private function loseExcessInventoryAtSea(): void {
+		$sorted    = [];
+		$silver    = 0.0;
+		$inventory = $this->unit->Inventory();
+		$person    = $this->calculus()->payload(1);
+		foreach ($inventory as $quantity) {
+			$commodity = $quantity->Commodity();
+			$weight    = $commodity->Weight();
+			if ($weight > $person || !$this->canKeep($commodity)) {
+				$inventory->remove($quantity);
+				$this->giftToRandomUnit($quantity);
+			} else {
+				$sorted[$weight][] = $quantity;
+				if ($commodity instanceof Silver) {
+					$silver = $quantity->Count() / $this->unit->Size();
+				}
+			}
+		}
+		ksort($sorted);
+
+		$payload = $this->calculus()->payload();
+		foreach ($sorted as $quantity) {
+			/** @var Quantity $quantity */
+			$commodity = $quantity->Commodity();
+			if ($commodity instanceof Silver) {
+				if ($silver > 100.0) {
+					$count = 100 * $this->unit->Size();
+					$lose  = $quantity->Count() - $count;
+					$inventory->remove(new Quantity($commodity, $lose));
+					$this->giftToRandomUnit(new Quantity($commodity, $lose));
+					$quantity = new Quantity($commodity, $count);
+				}
+			}
+			if ($quantity->Weight() > $payload) {
+				$count = $quantity->Count() - (int)floor($payload / $commodity->Weight());
+				$inventory->remove(new Quantity($commodity, $count));
+				$this->giftToRandomUnit(new Quantity($commodity, $count));
+			}
+			$payload -= $quantity->Weight();
+		}
+	}
+
+	private function canKeep(Commodity $commodity): bool {
+		if ($commodity instanceof Luxury) {
+			return false;
+		}
+		if ($commodity instanceof Protection) {
+			return false;
+		}
+		if ($commodity instanceof RawMaterial) {
+			return false;
+		}
+		return true;
 	}
 }
