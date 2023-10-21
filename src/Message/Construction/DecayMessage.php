d80640ad2109bf3451ffac8a8597995703d59c57
lemuria-pbem <lemuria@online.de>
2023-10-22 00:49:17 +0200

    [BUGFIX] Fix decay message translation.

--- src/Message/Construction/DecayMessage.php
+++ src/Message/Construction/DecayMessage.php
@@ -2,6 +2,8 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Message\Construction;
 
+use Lemuria\Engine\Fantasya\Message\Casus;
+use Lemuria\Engine\Fantasya\Message\LemuriaMessage;
 use Lemuria\Engine\Message\Result;
 use Lemuria\Singleton;
 
@@ -13,5 +15,14 @@ class DecayMessage extends AbstractConstructionMessage
 
 	protected function create(): string {
 		return 'The ravages of time let the ' . $this->building . ' ' . $this->id . ' decay.';
+	}
+
+	protected function getData(LemuriaMessage $message): void {
+		parent::getData($message);
+		$this->building = $message->getSingleton();
+	}
+
+	protected function getTranslation(string $name): string {
+		return $this->singleton($name, 'building') ?? parent::getTranslation($name);
 	}
 }
