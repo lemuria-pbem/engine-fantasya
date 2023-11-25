59ee849009349fad1eb4e9476ad2c07924a3f955
lemuria-pbem <lemuria@online.de>
2023-11-25 12:49:40 +0100

    [BUGFIX] Fix commerce not stopping LERNEN.

--- src/Factory/CommerceActivityTrait.php
+++ src/Factory/CommerceActivityTrait.php
@@ -3,17 +3,16 @@ declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Factory;
 
 use Lemuria\Engine\Fantasya\Activity;
+use Lemuria\Engine\Fantasya\Command\CommerceCommand;
 
 trait CommerceActivityTrait
 {
 	use DefaultActivityTrait;
 
-	private bool $isCommerceActivity = true;
-
 	/**
 	 * Allow execution of commerce activities.
 	 */
 	public function allows(Activity $activity): bool {
-		return isset($this->isCommerceActivity);
+		return $activity instanceof CommerceCommand;
 	}
 }
