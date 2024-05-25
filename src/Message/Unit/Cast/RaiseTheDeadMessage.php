e709cffa6fc87187e4e111b692a61eecb8567981
lemuria-pbem <lemuria@online.de>
2024-05-25 08:58:32 +0200

    [BUGFIX] Fix raise the dead message.

--- src/Message/Unit/Cast/RaiseTheDeadMessage.php
+++ src/Message/Unit/Cast/RaiseTheDeadMessage.php
@@ -2,6 +2,8 @@
 declare(strict_types = 1);
 namespace Lemuria\Engine\Fantasya\Message\Unit\Cast;
 
+use function Lemuria\number;
+use Lemuria\Engine\Fantasya\Message\Casus;
 use Lemuria\Engine\Fantasya\Message\LemuriaMessage;
 use Lemuria\Engine\Message\Result;
 use Lemuria\Id;
@@ -26,6 +28,9 @@ class RaiseTheDeadMessage extends AbstractCastMessage
 	}
 
 	protected function getTranslation(string $name): string {
-		return $this->item($name, 'gang') ?? parent::getTranslation($name);
+		if ($name === 'gang') {
+			return number($this->gang->Count()) . ' ' . $this->translateItem($this->gang, Casus::Accusative);
+		}
+		return parent::getTranslation($name);
 	}
 }
